2521936aa312f3eb2294c44275ecc4ea27d54bbe
ismo24 <ismaelkoura@gmail.com>
2024-11-21 15:40:49 +0000

    modification serveur

--- ConcoursDBServer.js
+++ ConcoursDBServer.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/download-ConcoursBF', (req, res) => {
+app.get('/Concours/download-ConcoursBF', (req, res) => {
     const dbPath = path.join(__dirname, '..', 'Databases', 'BURKINA_FASO', 'ConcoursBF.db');
    res.download(dbPath);
 });
